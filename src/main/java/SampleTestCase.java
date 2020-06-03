72bfcaae1347d53f5eca52512727cd8ae9a0d845
dmitriiyashkin <dmitriiyashkin@gmail.com>
2020-06-03 21:35:22 +0300

    Test: check amount followers

--- src/main/java/SampleTestCase.java
+++ src/main/java/SampleTestCase.java
@@ -1,13 +1,28 @@
 import models.User;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
 public class SampleTestCase {
+    private RestTemplate restTemplate;
+    private final String API_BASE = "https://api.github.com";
+
+    @Before
+    public void setUp(){
+        restTemplate = new RestTemplate();
+    }
+
     @Test
     public void testUserInfoResponse(){
-        RestTemplate restTemplate = new RestTemplate();
-        User user = restTemplate.getForObject("https://api.github.com/users/dmitriiyashkin", User.class);
-        System.out.println(user);
+        User remoteUser = restTemplate.getForObject(API_BASE + "/users/dmitriiyashkin", User.class);
+        Assert.assertTrue(remoteUser.getHtml_url().contains("https://github.com/dmitriiyashkin"));
+    }
+
+    @Test
+    public void testFollowers(){
+        User[] followers = restTemplate.getForObject(API_BASE + "/users/dmitriiyashkin/followers", User[].class);
+        Assert.assertTrue(followers.length == 0);
     }
 
 }
